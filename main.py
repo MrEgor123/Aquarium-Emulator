a89b49749839f3852095b5b1fd32f1e6fac43b36
MrEgor123 <144677952+MrEgor123@users.noreply.github.com>
2024-04-13 10:45:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -155,15 +155,25 @@ class MyWindow(QMainWindow):
         self.view.setViewportUpdateMode(QGraphicsView.FullViewportUpdate)
         self.view.setFrameShape(QFrame.NoFrame)
 
-        with open("/Users/mvideomvideo/Desktop/Python/Okun_description.txt", "r") as file:
+        with open(
+            "/Users/mvideomvideo/Desktop/Python/Okun_description.txt",
+                "r") as file:
             Okun_description = file.read()
-        with open("/Users/mvideomvideo/Desktop/Python/Shuka_description.txt", "r") as file:
+        with open(
+            "/Users/mvideomvideo/Desktop/Python/Shuka_description.txt",
+                "r") as file:
             Shuka_description = file.read()
-        with open("/Users/mvideomvideo/Desktop/Python/Carp_description.txt", "r") as file:
+        with open(
+            "/Users/mvideomvideo/Desktop/Python/Carp_description.txt",
+                "r") as file:
             Carp_description = file.read()
-        with open("/Users/mvideomvideo/Desktop/Python/Vobla_description.txt", "r") as file:
+        with open(
+            "/Users/mvideomvideo/Desktop/Python/Vobla_description.txt",
+                "r") as file:
             Vobla_description = file.read()
-        with open("/Users/mvideomvideo/Desktop/Python/Seld_description.txt", "r") as file:
+        with open(
+            "/Users/mvideomvideo/Desktop/Python/Seld_description.txt",
+                "r") as file:
             Seld_description = file.read()
 
         self.fishes = [
